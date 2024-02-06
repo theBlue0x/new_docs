f4ea79e7a82ca07f102b1896faa9205085f3fbbe
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2024-02-06 12:55:45 -0500

    Add Ubuntu font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)', ...fontFamily.sans],
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
         mono: [
           'Menlo',
           'Monaco',
