7380ee9b1cf2ff806d96987fcfd78b86c93ed61a
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2024-09-30 17:45:39 -0400

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full p-4 dark:bg-[#111111] md:flex md:items-center md:justify-between">
+    </footer>
+  );
+};
+export default Footer;
